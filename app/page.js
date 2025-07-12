82fb3ea8d0b9ddab20b9695ba388fa69a925affc
ankitsyscon <ankit@sysconinfotech.com>
2025-07-12 11:24:04 +0530

    adding navigation bar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
 import { countStore } from "@/store";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
+
   const { count, increase, reset } = countStore();
   console.log("Hello World")
   return (
+    <>
+    <Navbar />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -21,5 +25,6 @@ export default function Home() {
         <button onClick={() => reset()}>reset</button>
       </main>
     </div>
+    </>
   );
 }
