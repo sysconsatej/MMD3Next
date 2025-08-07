2a9e3bcfaa13c01b1b48d73fb11d4929c415f3b0
ankitsyscon <ankit@sysconinfotech.com>
2025-08-07 11:40:30 +0530

    merge conflit resolve

--- app/master/containerStatus/page.js
+++ app/master/containerStatus/page.js
@@ -8,7 +8,7 @@ import { theme } from "@/styles";
 import { toast, ToastContainer } from "react-toastify";
 import CustomButton from "@/components/button/button";
 
-export default function containerStatus() {
+export default function ContainerStatus() {
   const [formData, setFormData] = useState({
     containerDetails: [],
   });
