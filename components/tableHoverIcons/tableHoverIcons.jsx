8d48594460ab83beaa58643d4b3a83b3da5a3f98
ankitsyscon <ankit@sysconinfotech.com>
2025-08-20 14:48:17 +0530

    adding edit ,view,delete icons on table

---
+++ components/tableHoverIcons/tableHoverIcons.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import { Edit, Delete, Visibility } from "@mui/icons-material";
+
+export const HoverActionIcons = ({ onView, onEdit, onDelete }) => {
+  return (
+    <Box className="flex justify-end gap-2">
+      <Tooltip title="View" arrow>
+        <IconButton onClick={onView} sx={{ color: "#ffc400" }} size="small">
+          <Visibility fontSize="small" />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Edit" arrow>
+        <IconButton onClick={onEdit} sx={{ color: "#ffc400" }} size="small">
+          <Edit fontSize="small" />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Delete" arrow>
+        <IconButton onClick={onDelete} color="error" size="small">
+          <Delete fontSize="small" />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
