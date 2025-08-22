1e4a9e3e645a5177331f22aed5c152b38fbb8b7b
ankitsyscon <ankit@sysconinfotech.com>
2025-08-22 18:47:17 +0530

    nominatedarea,dpd,smtp,cfs for the submit ,edit,view

--- app/master/cfs/page.js
+++ app/master/cfs/page.js
@@ -1,14 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { ThemeProvider, Box } from "@mui/material";
 import data from "./cfsData";
 import { CustomInput } from "@/components/customInput";
 import { theme } from "@/styles";
 import { toast, ToastContainer } from "react-toastify";
 import CustomButton from "@/components/button/button";
+import { fetchForm, insertUpdateForm } from "@/apis";
+import { formatDataWithForm, formatFetchForm, formatFormData } from "@/utils";
+import { formStore } from "@/store";
 
-export default function City() {
+export default function Cfs() {
   const [formData, setFormData] = useState({
     containerDetails: [],
   });
@@ -17,10 +20,38 @@ export default function City() {
 
   const [jsonData, setJsonData] = useState(data);
 
+  const { mode, setMode } = formStore();
+
   const submitHandler = async (event) => {
     event.preventDefault();
-    toast.success("working!");
+    const format = formatFormData("tblPort", formData, mode.formId);
+    const { success, error, message } = await insertUpdateForm(format);
+    if (success) {
+      toast.success(message);
+      setFormData({});
+    } else {
+      toast.error(error || message);
+    }
   };
+
+  useEffect(() => {
+    async function fetchFormHandler() {
+      if (mode.formId) {
+        setFieldsMode(mode.mode);
+        const format = formatFetchForm(data, "tblPort", mode.formId);
+        const { success, result, message, error } = await fetchForm(format);
+        if (success) {
+          const getData = formatDataWithForm(result, data);
+          setFormData(getData);
+          toast.success(message);
+        } else {
+          toast.error(error || message);
+        }
+      }
+    }
+
+    fetchFormHandler();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -28,9 +59,13 @@ export default function City() {
         <section className="py-1 px-4">
           <Box className="flex justify-between items-end py-1">
             <h1 className="text-left text-base flex items-end m-0 ">
-               Container Freight Station    
+              Container Freight Station
             </h1>
-            <CustomButton text="Back" href="/master/cfs/list" />
+            <CustomButton
+              text="Back"
+              href="/master/cfs/list"
+              onClick={() => setMode({ mode: null, formId: null })}
+            />
           </Box>
           <Box className="border border-solid border-black rounded-[4px] ">
             <Box className="sm:grid sm:grid-cols-5 gap-2 flex flex-col p-1 border-b border-b-solid border-b-black ">
@@ -43,7 +78,9 @@ export default function City() {
             </Box>
           </Box>
           <Box className="w-full flex mt-2 ">
-            <CustomButton text={"Submit"} type="submit" />
+            {fieldsMode !== "view" && (
+              <CustomButton text={"Submit"} type="submit" />
+            )}
           </Box>
         </section>
       </form>
--- app/master/dpd/page.js
+++ app/master/dpd/page.js
@@ -1,12 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { ThemeProvider, Box } from "@mui/material";
 import data from "./dpdData";
 import { CustomInput } from "@/components/customInput";
 import { theme } from "@/styles";
 import { toast, ToastContainer } from "react-toastify";
 import CustomButton from "@/components/button/button";
+import { fetchForm, insertUpdateForm } from "@/apis";
+import { formatDataWithForm, formatFetchForm, formatFormData } from "@/utils";
+import { formStore } from "@/store";
 
 export default function DPD() {
   const [formData, setFormData] = useState({
@@ -17,11 +20,38 @@ export default function DPD() {
 
   const [jsonData, setJsonData] = useState(data);
 
+  const { mode, setMode } = formStore();
+
   const submitHandler = async (event) => {
     event.preventDefault();
-    toast.success("working!");
+    const format = formatFormData("tblPort", formData, mode.formId);
+    const { success, error, message } = await insertUpdateForm(format);
+    if (success) {
+      toast.success(message);
+      setFormData({});
+    } else {
+      toast.error(error || message);
+    }
   };
 
+  useEffect(() => {
+    async function fetchFormHandler() {
+      if (mode.formId) {
+        setFieldsMode(mode.mode);
+        const format = formatFetchForm(data, "tblPort", mode.formId);
+        const { success, result, message, error } = await fetchForm(format);
+        if (success) {
+          const getData = formatDataWithForm(result, data);
+          setFormData(getData);
+          toast.success(message);
+        } else {
+          toast.error(error || message);
+        }
+      }
+    }
+
+    fetchFormHandler();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <form onSubmit={submitHandler}>
@@ -30,7 +60,11 @@ export default function DPD() {
             <h1 className="text-left text-base flex items-end m-0 ">
               DPD Form
             </h1>
-            <CustomButton text="Back" href="/master/dpd/list" />
+            <CustomButton
+              text="Back"
+              href="/master/dpd/list"
+              onClick={() => setMode({ mode: null, formId: null })}
+            />
           </Box>
           <Box className="border border-solid border-black rounded-[4px] ">
             <Box className="sm:grid sm:grid-cols-5 gap-2 flex flex-col p-1 border-b border-b-solid border-b-black ">
@@ -43,7 +77,9 @@ export default function DPD() {
             </Box>
           </Box>
           <Box className="w-full flex mt-2 ">
-            <CustomButton text={"Submit"} type="submit" />
+            {fieldsMode !== "view" && (
+              <CustomButton text={"Submit"} type="submit" />
+            )}
           </Box>
         </section>
       </form>
