0df8a2d25fbf81ac47ba3e69050e3cdd2c09fed2
ankitsyscon <ankit@sysconinfotech.com>
2025-09-09 13:51:23 +0530

    chaning in navbar ,global css and creating new screen for the reports

---
+++ app/reports/customAndPort/igmEdi/igmData.js
@@ -0,0 +1,30 @@
+const fieldData = {
+  igmEdiFields: [
+    {
+      label: "Vessel",
+      name: "name",
+      type: "dropdown",
+      labelType: "vessel",
+      isEdit: "true",
+      required: true,
+    },
+    {
+      label: "Voyage",
+      name: "stateId",
+      type: "dropdown",
+      labelType: "voyage",
+      selectedCondition: "name",
+      isEdit: "true",
+    },
+
+    {
+      label: "POD",
+      name: "countryId",
+      type: "dropdown",
+      labelType: "port",
+      isEdit: "true",
+    },
+  ],
+};
+
+export default fieldData;
---
+++ app/reports/customAndPort/igmEdi/page.js
@@ -0,0 +1,79 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ThemeProvider, Box } from "@mui/material";
+import data from "./igmData";
+import { CustomInput } from "@/components/customInput";
+import { theme } from "@/styles";
+import { toast, ToastContainer } from "react-toastify";
+import CustomButton from "@/components/button/button";
+import { formStore } from "@/store";
+import { fetchForm, insertUpdateForm } from "@/apis";
+import { formatDataWithForm, formatFetchForm, formatFormData } from "@/utils";
+
+export default function IGMEDI() {
+  const [formData, setFormData] = useState({});
+  const [fieldsMode, setFieldsMode] = useState("");
+  const [jsonData, setJsonData] = useState(data);
+  const { mode, setMode } = formStore();
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const format = formatFormData("tblCity", formData, mode.formId);
+    const { success, error, message } = await insertUpdateForm(format);
+    if (success) {
+      toast.success(message);
+      setFormData({});
+    } else {
+      toast.error(error || message);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchFormHandler() {
+      if (mode.formId) {
+        setFieldsMode(mode.mode);
+        const format = formatFetchForm(data, "tblCity", mode.formId);
+        const { success, result, message, error } = await fetchForm(format);
+        if (success) {
+          const getData = formatDataWithForm(result, data);
+          setFormData(getData);
+          toast.success(message);
+        } else {
+          toast.error(error || message);
+        }
+      }
+    }
+    fetchFormHandler();
+  }, [mode.formId]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <form onSubmit={submitHandler}>
+        <section className="py-1 px-4">
+          <Box className="flex justify-between items-end py-1">
+            <h1 className="text-left text-base flex items-end m-0 ">IGM EDI</h1>
+          </Box>
+          <Box className="border border-solid border-black rounded-[4px] ">
+            <Box className="sm:grid sm:grid-cols-3 gap-2 flex flex-col p-1 border-b border-b-solid border-b-black ">
+              <CustomInput
+                fields={jsonData.igmEdiFields}
+                formData={formData}
+                setFormData={setFormData}
+                fieldsMode={fieldsMode}
+              />
+            </Box>
+          </Box>
+          <Box className="w-full flex mt-2  gap-2">
+            {fieldsMode !== "view" && (
+              <CustomButton text={"GET BL DETAILS"} type="submit" />
+            )}
+            {fieldsMode !== "view" && (
+              <CustomButton text={"GENERATE REPORT"} type="submit" />
+            )}
+          </Box>
+        </section>
+      </form>
+      <ToastContainer />
+    </ThemeProvider>
+  );
+}
