064cecc6eeac3860c3d8797121e2a070ca854080
Abdullah Ansari <abdullah.sysconinfotech@gmail.com>
2025-09-18 17:50:01 +0530

    file input name issue resolved

--- components/customInput/fileInput.js
+++ components/customInput/fileInput.js
@@ -69,7 +69,8 @@ const FileInput = ({
               variant="body2"
               className="!text-[11px] whitespace-nowrap overflow-hidden overflow-ellipsis"
             >
-              {fieldValue?.name?.split("-")[1] || fieldValue?.split("-")[1]}
+              {fieldValue?.name?.split(/-(.+)/)[1] ||
+                fieldValue?.split(/-(.+)/)[1]}
             </Typography>
           </Box>
           <IconButton size="small" onClick={fileCancelHandler}>
