e73755782e916caa6adc9fddc5544ecb91bd6b63
tejassysconinfotech <tejas@sysconinfotech.com>
2025-09-30 15:01:13 +0530

    merge resolve

--- apis/index.js
+++ apis/index.js
@@ -4,10 +4,7 @@ export {
   getNextPrevData,
 } from "./utils";
 export { fetchTableValues } from "./dynamicTable";
-<<<<<<< HEAD
-export { insertUpdateForm, fetchForm, deleteRecord } from "./dynamicForm";
 export { updateStatusRows } from "./updateStatus";
-=======
 export {
   insertUpdateForm,
   fetchForm,
@@ -19,4 +16,3 @@ export {
   updateDynamicReportData,
 } from "./dynamicReport";
 export { sendEmail } from "./sendEmail";
->>>>>>> 2b294c36ed64acc6fbefdc6aec0bca94f71f7693
