11fb5c0abcb09f07d7b3bc41565921e67a07c4e1
aakashyadav-sys <aakashyadav@sysconinfotech.com>
2025-10-06 13:00:21 +0530

    testing  push

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 export default function Home() {
+  console.log("Testing pushing code ....")
   return (
     <>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
