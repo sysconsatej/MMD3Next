16f49e0617b8d33a01feec7592030e8eba15d7d1
tejassysconinfotech <tejas@sysconinfotech.com>
2025-10-11 14:05:43 +0530

    dropdown data page

--- app/reports/customAndPort/cargoArrivalNotice/cargoArrivalNoticeData.js
+++ app/reports/customAndPort/cargoArrivalNotice/cargoArrivalNoticeData.js
@@ -4,33 +4,53 @@ const fieldData = {
       label: "Vessel",
       name: "vessel",
       type: "dropdown",
+      tableName: "tblVessel t",
+      idColumn: "id",
+      displayColumn: "t.name",
+      searchColumn: "t.name",
+      orderBy: "t.name",
       labelType: "vessel",
-      isEdit: "true",
+      isEdit: true
     },
     {
       label: "Voyage",
       name: "voyage",
       type: "dropdown",
-      labelType: "voyage",
+      tableName: "tblVoyage t",
+      idColumn: "id",
+      displayColumn: "t.voyageNo",
+      searchColumn: "t.voyageNo",
       selectedCondition: "vessel",
-      isEdit: "true",
+      where: "(@selectedCondition IS NULL OR t.vesselId = @selectedCondition)",
+      orderBy: "t.voyageNo",
+      labelType: "voyage",
+      isEdit: true
     },
-
     {
       label: "POD",
       name: "pod",
       type: "dropdown",
+      tableName: "tblPort t",
+      idColumn: "id",
+      displayColumn: "t.name",
+      searchColumn: "t.name",
+      orderBy: "t.name",
       labelType: "port",
-      isEdit: "true",
+      isEdit: true
     },
     {
       label: "FPD",
       name: "terminal",
       type: "dropdown",
-      labelType: "pod",
-      isEdit: "true",
+      tableName: "tblPort t",
+      idColumn: "id",
+      displayColumn: "t.name",
+      searchColumn: "t.name",
+      orderBy: "t.name",
+      labelType: "port",
+      isEdit: true
     },
   ],
 };
 export default fieldData;
-export const metaData=[];
+export const metaData = [];
