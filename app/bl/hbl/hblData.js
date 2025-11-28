cf9d1aa5d0a01aae17e4a5fb50a7260a9c226ed7
aakasyadav-sys <aakasyadav@sysconinfotech.com>
2025-11-28 15:53:58 +0530

    code updated

--- app/bl/hbl/hblData.js
+++ app/bl/hbl/hblData.js
@@ -714,18 +714,18 @@ export const fieldData = {
       isEdit: true,
       type: "checkbox",
     },
-    {
-      label: "Custom Seal",
-      // name: "containerAgentCode",
-      isEdit: true,
-      // required: true,
-    },
-    {
-      label: "Agent Seal",
-      // name: "containerAgentCode",
-      isEdit: true,
-      // required: true,
-    },
+    // {
+    //   label: "Custom Seal",
+    //   // name: "containerAgentCode",
+    //   isEdit: true,
+    //   // required: true,
+    // },
+    // {
+    //   label: "Agent Seal",
+    //   // name: "containerAgentCode",
+    //   isEdit: true,
+    //   // required: true,
+    // },
     {
       label: "Container Agent Code",
       name: "containerAgentCode",
