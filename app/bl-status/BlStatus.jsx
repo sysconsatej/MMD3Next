6a1e4374bcf2dab13736137dfd762bd1b2bc4f83
aakasyadav-sys <aakasyadav@sysconinfotech.com>
2025-12-06 17:09:38 +0530

    added bl Status api bind with frontend

--- app/bl-status/BlStatus.jsx
+++ app/bl-status/BlStatus.jsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import HorizontalLinearStepper from "./Wizard";
 import { CustomInput } from "@/components/customInput";
-import { useEffect, useState } from "react";
-import { useDebounce } from "@/utils";
-import { getDataWithCondition } from "@/apis";
+import { useState } from "react";
+import CustomButton from "@/components/button/button";
+import { useBlWorkFlowData } from "@/store";
+import { toast, ToastContainer } from "react-toastify";
 
 const inputField = {
   fieldsData: [
     {
       name: "search",
       type: "text",
-      label: "Enter Bl No",
-      changeFunc: "getBlNoDetails",
+      label: "Bl No",
     },
   ],
 };
@@ -24,67 +24,57 @@ const processArr = [
     cardType: "Request",
     bgColor: "#f47b20",
     status: "off",
+    keyName: "invoiceRequest",
+    link :  "/invoice/invoiceRequest"
   },
   {
     id: 2,
     cardType: "Invoices",
     bgColor: "#d47add",
     status: "off",
+    keyName: "invoice",
+    // link :  "/invoice/invoiceRelease",
   },
   {
     id: 3,
     cardType: "Payments",
     bgColor: "#33c6f4",
     status: "off",
+    keyName: "invoicePayment",
+    // link :  "",
   },
   {
     id: 4,
     cardType: "Do",
     bgColor: "#a56cc1",
     status: "off",
-  },
-  {
-    id: 5,
-    cardType: "Receipts",
-    bgColor: "#f8cf3e",
-    status: "off",
+    keyName: "do",
+    // link :  "",
   },
 ];
 
 export const BlStatus = () => {
   const [formData, setFormData] = useState({ search: "" });
-
-  const debounceSearch = useDebounce(formData.search, 300);
-
-  const fetchData   =  async()  =>  {
-    // const 
-    const res  =  await getDataWithCondition()
-  }
-
-  useEffect(() => {
-    if (debounceSearch) {
-    }
-  }, [debounceSearch]);
-
-  const handleChange = {
-    getBlNoDetails: async (name, value) => {
-      setFormData((prev) => {
-        return { ...prev, [name]: String(value).trim().toLowerCase() };
-      });
-    },
+  const { fetchData } = useBlWorkFlowData();
+  const getBlWorkFlowData = () => {
+    if (!formData.search) return toast.error("Please Enter Bl NO");
+    fetchData({ blNo: formData.search });
   };
 
   return (
-    <>
-      <CustomInput
-        fields={inputField.fieldsData}
-        formData={formData}
-        setFormData={setFormData}
-        handleChangeEventFunctions={handleChange}
-      />
+    <Container>
+      <Box className="flex gap-4 mt-3">
+        <CustomInput
+          fields={inputField.fieldsData}
+          formData={formData}
+          setFormData={setFormData}
+        />
+        <CustomButton onClick={getBlWorkFlowData} text={"Search"} />
+      </Box>
       <Box className="mt-5 px-5">
         <HorizontalLinearStepper steps={processArr} />
       </Box>
-    </>
+      <ToastContainer />
+    </Container>
   );
 };
