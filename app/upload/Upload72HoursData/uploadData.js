27b9b3a64ff55b4e479787a7d889c30ca86ac6f2
rohit-syscon <152374724+rohit-syscon@users.noreply.github.com>
2025-12-29 12:27:48 +0530

    Excel Upload

---
+++ app/upload/Upload72HoursData/uploadData.js
@@ -0,0 +1,18 @@
+const fieldData = {
+    mblFields: [
+         {
+            label: "Shipping Line",
+            name: "shippingLineId",
+            type: "dropdown",
+            tableName: "tblCompany c",
+            displayColumn: "c.name",
+            orderBy: "c.name",
+            foreignTable: "name,tblCompany",
+            isEdit: true,
+            required: true,
+        },
+        { label: "Upload File", name: "upload", type: "fileupload" },
+    ],
+};
+
+export default fieldData;
