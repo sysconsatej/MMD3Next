080b61af49224d62588cbd6c8d3919d5de89da68
ankitsyscon <ankit@sysconinfotech.com>
2026-02-23 19:05:05 +0530

    adding new payment report

---
+++ app/reports/customAndPort/customerPaymentRegister/customerPaymentRegisterData.js
@@ -0,0 +1,64 @@
+import { getUserByCookies } from "@/utils";
+import { Label } from "@mui/icons-material";
+const userData = getUserByCookies();
+const fieldData = {
+  igmGenerationFields: [
+    {
+      label: "Liner",
+      name: "shippingLineId",
+      type: "dropdown",
+      tableName: "tblCompany c",
+      idColumn: "id",
+      displayColumn: "c.name",
+      searchColumn: "c.name",
+      orderBy: "c.name",
+      isEdit: true,
+    },
+    {
+      label: "Bl No",
+      name: "blNo",
+      type: "text",
+      isEdit: true,
+    },
+    {
+      label: "Invoice No",
+      name: "invoiceNo",
+      type: "text",
+      isEdit: true,
+    },
+    {
+      label: "Payment Type",
+      name: "paymentModeId",
+      type: "dropdown",
+      tableName: "tblMasterData m",
+      where: "m.masterListName = 'tblPaymentType' AND m.name <> 'Receipt'",
+      displayColumn: "m.name",
+      foreignTable: "name,tblMasterData",
+      isEdit: true,
+    },
+    {
+      label: "Status",
+      name: "status",
+      type: "dropdown",
+      tableName: "tblMasterData m",
+      displayColumn: "m.name",
+      where: "m.masterListName = 'tblPaymentStatus'",
+      orderBy: "m.name",
+      isEdit: true,
+    },
+    {
+      label: "From Date",
+      name: "fromDate",
+      type: "date",
+      isEdit: true,
+    },
+    {
+      label: "To Date ",
+      name: "toDate",
+      type: "date",
+      isEdit: true,
+    },
+  ],
+};
+export default fieldData;
+export const metaData = [];
