cff5dc078b261306bb2e02100f374ef79cf85877
Abdullah Ansari <abdullah.sysconinfotech@gmail.com>
2026-04-02 11:36:16 +0530

    mbl transhipment dropdown changes

--- app/bl/mbl/mblData.js
+++ app/bl/mbl/mblData.js
@@ -210,6 +210,9 @@ export const fieldData = {
       type: "dropdown",
       tableName: "tblPort p",
       displayColumn: "ISNULL(p.code,'') + ' - ' + ISNULL(p.name,'')",
+      joins:
+        "JOIN tblMasterData m ON m.id = p.portTypeId JOIN tblCountry c ON c.id = p.countryId",
+      where: "m.name IN ('SEA PORT','INLAND PORT')",
       orderBy: "p.name",
       foreignTable: "code-name,tblPort",
       isEdit: true,
@@ -220,6 +223,9 @@ export const fieldData = {
       type: "dropdown",
       tableName: "tblPort p",
       displayColumn: "ISNULL(p.code,'') + ' - ' + ISNULL(p.name,'')",
+      joins:
+        "JOIN tblMasterData m ON m.id = p.portTypeId JOIN tblCountry c ON c.id = p.countryId",
+      where: "m.name IN ('SEA PORT','INLAND PORT')",
       orderBy: "p.name",
       foreignTable: "code-name,tblPort",
       isEdit: true,
